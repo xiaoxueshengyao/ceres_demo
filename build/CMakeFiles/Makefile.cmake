6fd28b7ffecb7007e2c67c4075b69460e918aea8
xiaoxueshengyao <yjj6027805@163.com>
2022-08-07 19:26:18 +0800

    curve fitting

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -61,5 +61,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/curvefitting.dir/DependInfo.cmake"
   "CMakeFiles/powellfunc.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/curvefitting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curvefitting.dir/src/curve_fitting.cpp.o"
+  "curvefitting"
+  "curvefitting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curvefitting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
